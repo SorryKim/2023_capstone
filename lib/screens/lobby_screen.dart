66824c21bc26b70aae5a53ac5a1b7a8d5d3fd955
lee <leechaerim214@gmail.com>
2023-05-18 21:50:03 +0900

    로비 애니메이션 수정

--- lib/screens/lobby_screen.dart
+++ lib/screens/lobby_screen.dart
@@ -1,9 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_plus/flutter_swiper_plus.dart';
 import 'package:project/models/mountains_model.dart';
 import 'package:project/screens/information_screen.dart';
 import 'package:project/screens/login_screen.dart';
+
+final List<String> imgList = [
+  'images/6249016.jpg',
+  'images/6229893.jpg',
+];
 
 class LobbyScreen extends StatefulWidget {
   final String uid;
@@ -26,109 +32,104 @@ class _LobbyScreenState extends State<LobbyScreen> {
             return const LoginScreen();
           } else {
             return Scaffold(
-              appBar: AppBar(
-                title: const Text(
-                  'MOUNTAINDEW',
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Color.fromARGB(255, 10, 68, 12),
-                    fontWeight: FontWeight.bold,
+                appBar: AppBar(
+                  title: const Text(
+                    'MOUNTAINDEW',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color.fromARGB(255, 10, 68, 12),
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                  actions: [
+                    IconButton(
+                      icon: const Icon(Icons.person),
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                InformationScreen(uid: widget.uid)));
+                      },
+                      color: const Color.fromARGB(255, 10, 68, 12),
+                      iconSize: 40,
+                    )
+                  ],
                 ),
-                backgroundColor: Colors.transparent,
-                elevation: 0.0,
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.person),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) =>
-                              InformationScreen(uid: widget.uid)));
-                    },
-                    color: const Color.fromARGB(255, 10, 68, 12),
-                    iconSize: 40,
-                  )
-                ],
-              ),
-              body: StreamBuilder(
-                  stream: streamMountains(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return const Center(
-                        child: Text("오류발생~"),
-                      );
-                    } else {
-                      return SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              AnimatedCrossFade(
-                                duration: const Duration(milliseconds: 500),
-                                firstChild: Image.asset('images/6249016.jpg'),
-                                secondChild: Image.asset('images/6229893.jpg'),
-                                crossFadeState: selected
-                                    ? CrossFadeState.showFirst
-                                    : CrossFadeState.showSecond,
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              const Text('사용자 맞춤 등산로 추천 1'),
-                              Container(
-                                height: 100,
-                                width: 520,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(
-                                    color:
-                                        const Color.fromARGB(255, 10, 68, 12),
-                                    style: BorderStyle.solid,
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                              const Text('사용자 맞춤 등산로 추천 2'),
-                              Container(
-                                height: 100,
-                                width: 520,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(
-                                    color:
-                                        const Color.fromARGB(255, 10, 68, 12),
-                                    style: BorderStyle.solid,
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                              const Text('사용자 맞춤 등산로 추천 3'),
-                              Container(
-                                height: 100,
-                                width: 520,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: Border.all(
-                                    color:
-                                        const Color.fromARGB(255, 10, 68, 12),
-                                    style: BorderStyle.solid,
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                            ],
+                body: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          color: Colors.white,
+                          height: 280,
+                          child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Swiper(
+                                autoplay: true,
+                                scale: 0.9,
+                                viewportFraction: 0.8,
+                                pagination: const SwiperPagination(
+                                    alignment: Alignment.bottomRight),
+                                itemCount: imgList.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Image.asset(imgList[index]);
+                                },
+                              )),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text('사용자 맞춤 등산로 추천 1'),
+                        Container(
+                          height: 100,
+                          width: 520,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 10, 68, 12),
+                              style: BorderStyle.solid,
+                              width: 2,
+                            ),
                           ),
                         ),
-                      );
-                    }
-                  }),
-            );
+                        const Text('사용자 맞춤 등산로 추천 2'),
+                        Container(
+                          height: 100,
+                          width: 520,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 10, 68, 12),
+                              style: BorderStyle.solid,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                        const Text('사용자 맞춤 등산로 추천 3'),
+                        Container(
+                          height: 100,
+                          width: 520,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 10, 68, 12),
+                              style: BorderStyle.solid,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ));
           }
         });
   }
